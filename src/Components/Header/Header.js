08a4b695d67a717988c5ab6407999a7fca9870b7
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2021-03-03 20:15:06 +0100

    First commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+
+    render () {
+    return (
+        <div>
+            <header>
+                <h1>Keeper App</h1>
+            </header>
+        </div>
+    )
+}
+}
---
+++ src/Components/Note/Note.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Note extends Component {
+    render() {
+        return (
+            <div>
+                <h1>TITLE</h1>
+                <p>CONTENT</p>
+            </div>
+        )
+    }
+}
